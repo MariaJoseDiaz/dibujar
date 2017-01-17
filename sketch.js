53a5d492bc07538104009b8dd5b586a83bc8bb64
MariaJoseDiaz <mjdiaz9@uc.cl>
2017-01-17 16:14:42 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 var ptouchIsDown;
-var pmouseIsPressed;
 function setup() {
   // uncomment this line to make the canvas the full size of the window
   createCanvas(windowWidth, windowHeight);
@@ -8,7 +7,6 @@ function setup() {
   fill(0);
   background(255);
   ptouchIsDown = touchIsDown;
-  pmouseIsPressed = mouseIsPressed;
 }
 
 function draw() {
@@ -16,27 +14,16 @@ function draw() {
     stroke(255, 0, 0);
     line(touchX, touchY, ptouchX, ptouchY);
   }
-  if (pmouseIsPressed && mouseIsPressed){
-    stroke(0, 0, 255);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
+  
   ptouchIsDown = touchIsDown;
-  pmouseIsPressed = mouseIsPressed;
 }
 
 function touchMoved(){
-  //stroke(255, 0, 0);
-  //line(touchX, touchY, ptouchX, ptouchY);
+
   return false;
 }
 
 function touchEnded(){
-  //stroke(0, 0, 255);
-  //line(touchX, touchY, ptouchX, ptouchY);
+ 
   return false;
-}
-
-function mouseDragged(){
-  //stroke(0, 255, 0);
-  //line(mouseX, mouseY, pmouseX, pmouseY);
 }
